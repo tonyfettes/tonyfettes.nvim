145def5dc8f27eae4a60af104d32229f209c1deb
Tony Fettes <tonyfettes@tonyfettes.com>
2024-01-14 16:26:36 +0800

    fix(find): telescope -> find

--- lua/plugins/telescope.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'nvim-telescope/telescope.nvim', tag = '0.1.4',
-  keys = {
-    { '<leader>f', '<Cmd>Telescope find_files<CR>', desc = 'Find Files' },
-  },
-  dependencies = {
-    'nvim-lua/plenary.nvim'
-  },
-}
