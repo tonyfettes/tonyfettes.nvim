bf936bd5f967634265635edc2a1444e699bc7f82
Tony Fettes <tonyfettes@tonyfettes.com>
2024-01-14 16:25:55 +0800

    fix(term)!: use <M-CR> to toggle term

--- lua/plugins/term.lua
+++ lua/plugins/term.lua
@@ -3,8 +3,10 @@ return {
     'akinsho/toggleterm.nvim',
     version = "*",
     keys = {
-      { '<M-t>', '<Cmd>ToggleTerm<CR>', desc = 'Toggle Terminal' }
+      { '<M-CR>', '<Cmd>ToggleTerm<CR>', desc = 'Toggle Terminal' }
     },
-    config = true
+    opts = {
+      open_mapping = [[<M-CR>]]
+    }
   }
 }
