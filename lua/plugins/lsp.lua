604b35965656c1848ec29dd07fc00c79edc6217c
Tony Fettes <tonyfettes@tonyfettes.com>
2024-10-08 20:25:17 +0800

    feat: add keymap for codelens

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -104,6 +104,7 @@ return {
           vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
           vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
           vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
+          vim.keymap.set('n', '<leader>cl', vim.lsp.codelens.run, {})
           vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
           vim.keymap.set('n', '<leader>=', function()
             vim.lsp.buf.format { async = true }
