ebd1450f2dbf10f5ae59d9f2e3dddd1e538ae7b2
Tony Fettes <tonyfettes@tonyfettes.com>
2025-03-30 00:28:57 +0800

    feat(tree): add smart toggle & cd

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -1,14 +1,46 @@
 return {
   'nvim-tree/nvim-tree.lua',
   keys = {
-    { '<Leader>e', '<Cmd>NvimTreeToggle<CR>', desc = "Open Tree" },
-    { '<M-e>',     '<Cmd>NvimTreeToggle<CR>', desc = "Open Tree" }
+    { '<Leader>e', '<Cmd>NvimTreeFindFile<CR>', desc = "Open Tree" },
   },
   config = function()
     vim.g.loaded_netrw = 1
     vim.g.loaded_netrwPlugin = 1
 
-    require 'nvim-tree'.setup {
+    local tree = require 'nvim-tree'
+    local api = require 'nvim-tree.api'
+
+    local function on_attach(bufnr)
+      api.config.mappings.default_on_attach(bufnr)
+
+      local function change_directory_to(node)
+        if node == nil then
+          node = api.tree.get_node_under_cursor()
+        end
+        if node ~= nil and node.type == "directory" then
+          vim.api.nvim_set_current_dir(node.absolute_path)
+        end
+        api.tree.change_root_to_node(node)
+      end
+      vim.keymap.set('n', '<C-]>', change_directory_to,
+        { buffer = bufnr, noremap = true, silent = true, desc = "Change Directory to Node" })
+    end
+
+    local function smart_toggle()
+      vim.notify('smart_toggle')
+      local buffer = vim.api.nvim_get_current_buf()
+      local filetype = vim.api.nvim_get_option_value("filetype", { buf = buffer })
+      if filetype == "NvimTree" then
+        api.tree.toggle()
+      else
+        api.tree.find_file({ open = true, focus = true })
+      end
+    end
+
+    vim.keymap.set('n', '<Leader>e', smart_toggle)
+
+    tree.setup {
+      on_attach = on_attach,
       renderer = {
         icons = {
           show = {
