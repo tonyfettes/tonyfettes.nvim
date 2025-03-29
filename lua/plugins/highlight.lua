70db365f27d8b2407b4dd082dae79133fdfaaa1e
Tony Fettes <tonyfettes@tonyfettes.com>
2025-03-30 00:29:25 +0800

    clean: remove tree-sitter playground

--- lua/plugins/highlight.lua
+++ lua/plugins/highlight.lua
@@ -13,30 +13,5 @@ return {
         }
       }
     end
-  },
-
-  {
-    "nvim-treesitter/playground",
-    cmd = "TSPlaygroundToggle",
-    config = function()
-      require 'nvim-treesitter.configs'.setup {
-        playground = {
-          enable = true,
-          disable = {},
-          keybindings = {
-            toggle_query_editor = 'o',
-            toggle_hl_groups = 'i',
-            toggle_injected_languages = 't',
-            toggle_anonymous_nodes = 'a',
-            toggle_language_display = 'I',
-            focus_language = 'f',
-            unfocus_language = 'F',
-            update = 'R',
-            goto_note = '<cr>',
-            show_help = '?',
-          }
-        }
-      }
-    end
-  },
+  }
 }
