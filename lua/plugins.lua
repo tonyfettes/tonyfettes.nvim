fe2fde5e2b969acd10397ab892fbe7308ebf0893
Tony Fettes <tonyfettes@tonyfettes.com>
2025-03-30 01:53:09 +0800

    refactor: move task and ai plugins out

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -33,40 +33,6 @@ return {
     ft = { 'moonbit' },
     opts = {}
   },
-
-  {
-    'stevearc/overseer.nvim',
-    keys = {
-      { '<leader>o', '<Cmd>OverseerToggle bottom<CR>', desc = 'Toggle Overseer' },
-    },
-    config = function()
-      local overseer = require 'overseer'
-      overseer.setup {}
-      vim.api.nvim_create_user_command(
-        'Make',
-        function(params)
-          -- Insert args at the '$*' in the makeprg
-          local cmd, num_subs = vim.o.makeprg:gsub('%$%*', params.args)
-          if num_subs == 0 then
-            cmd = cmd .. ' ' .. params.args
-          end
-          local task = overseer.new_task({
-            cmd = vim.fn.expandcmd(cmd),
-            components = {
-              { 'on_output_quickfix', open = not params.bang, open_height = 8 },
-              'default',
-            },
-          })
-          task:start()
-        end, {
-          desc = 'Run your makeprg as an Overseer task',
-          nargs = '*',
-          bang = true,
-        }
-      )
-    end
-  },
-
   {
     "nvim-neorg/neorg",
     dependencies = {
@@ -81,23 +47,6 @@ return {
   },
 
   {
-    'tomtomjhj/coq-lsp.nvim',
-    dependencies = {
-      { 'whonore/Coqtail', ft = { 'coq' } }
-    },
-    ft = { 'coq' },
-    config = function()
-      vim.g.loaded_coqtail = 1
-      vim.g["coqtail#supported"] = 0
-      require 'coq-lsp'.setup {}
-    end
-  },
-
-  {
-    "lervag/vimtex",
-  },
-
-  {
     "numToStr/Navigator.nvim",
     config = function()
       local navigator = require 'Navigator'
@@ -109,52 +58,4 @@ return {
       -- vim.keymap.set({ 'n', 't' }, '<C-p>', '<Cmd>NavigatorPrevious<CR>')
     end
   },
-
-  {
-    'mrcjkb/rustaceanvim',
-    version = '^4', -- Recommended
-    lazy = false,   -- This plugin is already lazy
-  },
-
-  {
-    'isti115/agda.nvim',
-    dependencies = {
-      'nvim-lua/plenary.nvim'
-    },
-    lazy = false,
-    config = function()
-      vim.g.agda_theme = 'light'
-    end
-  },
-
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
-    config = function()
-      require "copilot".setup()
-    end,
-  },
-
-  {
-    "yetone/avante.nvim",
-    event = "VeryLazy",
-    lazy = false,
-    version = false, -- set this if you want to always pull the latest change
-    opts = {
-      -- add any opts here
-      provider = 'copilot'
-    },
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    build = "make",
-    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-    dependencies = {
-      "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      --- The below dependencies are optional,
-      "hrsh7th/nvim-cmp",       -- autocompletion for avante commands and mentions
-      "zbirenbaum/copilot.lua", -- for providers='copilot'
-    },
-  }
 }
---
+++ lua/plugins/ai.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require "copilot".setup()
+    end,
+  },
+
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = false,
+    version = false, -- set this if you want to always pull the latest change
+    opts = {
+      -- add any opts here
+      provider = 'copilot'
+    },
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = "make",
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    dependencies = {
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "hrsh7th/nvim-cmp",       -- autocompletion for avante commands and mentions
+      "zbirenbaum/copilot.lua", -- for providers='copilot'
+    },
+  }
+}
