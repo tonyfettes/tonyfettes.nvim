703f52804f35a2c3dc16124010d4c4afefc4e5e5
Tony Fettes <tonyfettes@tonyfettes.com>
2025-03-30 01:42:30 +0800

    fix: disable smart toggle

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -1,7 +1,7 @@
 return {
   'nvim-tree/nvim-tree.lua',
   keys = {
-    { '<Leader>e', '<Cmd>NvimTreeFindFile<CR>', desc = "Open Tree" },
+    { '<Leader>e', '<Cmd>NvimTreeFindFileToggle<CR>', desc = "Open Tree" },
   },
   config = function()
     vim.g.loaded_netrw = 1
@@ -25,19 +25,6 @@ return {
       vim.keymap.set('n', '<C-]>', change_directory_to,
         { buffer = bufnr, noremap = true, silent = true, desc = "Change Directory to Node" })
     end
-
-    local function smart_toggle()
-      vim.notify('smart_toggle')
-      local buffer = vim.api.nvim_get_current_buf()
-      local filetype = vim.api.nvim_get_option_value("filetype", { buf = buffer })
-      if filetype == "NvimTree" then
-        api.tree.toggle()
-      else
-        api.tree.find_file({ open = true, focus = true })
-      end
-    end
-
-    vim.keymap.set('n', '<Leader>e', smart_toggle)
 
     tree.setup {
       on_attach = on_attach,
