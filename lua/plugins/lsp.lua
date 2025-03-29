d714806c7d810271058a4a9bd1777e53da6dda93
Tony Fettes <tonyfettes@tonyfettes.com>
2025-03-30 01:35:24 +0800

    clean: remove comments

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -76,12 +76,7 @@ return {
               checkThirdParty = false,
               library = {
                 vim.env.VIMRUNTIME
-                -- Depending on the usage, you might want to add additional paths here.
-                -- "${3rd}/luv/library"
-                -- "${3rd}/busted/library",
-              }
-              -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-              -- library = vim.api.nvim_get_runtime_file("", true)
+              },
             }
           })
         end,
